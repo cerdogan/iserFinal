295029de82bae796279de93802b8d850ac1a5a68
Can Erdogan <canerdogan.89@gmail.com>
2014-06-04 18:34:29 -0400

    asdf

---
+++ src/manipulation.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file manipulation.cpp
+ * @author Can Erdogan
+ * @date Jun 04, 2014
+ * @brief Handles manipulation, aka motion planning, hardcoded behaviors, etc.
+ */
+
+#include "manipulation.h"
+
+bool manipulation (Mode mode) {
+
+}
---
+++ src/manipulation.h
@@ -0,0 +1,14 @@
+/**
+ * @file manipulation.h
+ * @author Can Erdogan
+ * @date Jun 04, 2014
+ * @brief Handles manipulation, aka motion planning, hardcoded behaviors, etc.
+ */
+
+#pragma once
+
+#include "helpers.h"
+
+/// Looks for a certian object and sets its position
+bool manipulation (Mode mode); 
+
